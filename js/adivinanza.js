51f0e465812f70de86cc745cf7a675333582f055
wooobotch <abraham.chalave@outlook.com>
2020-09-07 18:13:26 -0300

    javascript corregido. Intercambio entre elementos de footer y de head

--- js/adivinanza.js
+++ js/adivinanza.js
@@ -1,7 +1,43 @@
 let intentos = 0;
 
-let boton = document.getElementById("boton");
+let aprobado = document.getElementById("test_d");
+let falso_1 = document.getElementById("test_a");
+let falso_2 = document.getElementById("test_b");
+let falso_3 = document.getElementById("test_c");
 
-boton.onclick = function(){
-    intentos<4?intentos++:intentos=0;
+let pista = function(n){
+    switch(n){
+        case 1:
+            alert("La correcta es otra que no hayas intentado.");
+            break;
+        case 2:
+            alert("Queda una, no hay mucho que probar.");
+            break;
+        default:
+            break;
+    }
 }
+
+let setIntento = function(nombre){
+    if (nombre=="correcto" || intentos==4){
+        intentos = 0;
+    }else{
+        pista(intentos);
+        intentos +=1;
+    }
+};
+
+aprobado.onclick = function() {
+    setIntento("correcto");
+    alert("Todo piola, adivinaste!!");
+  };
+
+falso_1.onclick= function(){
+    setIntento("malo");
+};
+falso_2.onclick = function(){
+    setIntento("malo");
+};
+falso_3.onclick = function(){
+    setIntento("malo");
+};
